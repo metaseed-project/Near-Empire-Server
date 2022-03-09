3771995605615a4fbca6f7e39729fea56e12df50
ivan <vivan.volovik@gmail.com>
2022-03-09 12:10:15 +0200

    +

--- GalaxyServer/src/index.js
+++ GalaxyServer/src/index.js
@@ -19,9 +19,9 @@ app.get('/addMiner', async (req, res) => {
   const name = req.query.name;
   const clanId = req.query.clanId;
   const mineType = req.query.mineType;
-  
+
   const planet = await planetsSchema.findOne({ name });
-  if(!planet) res.send({ err: "no planet found" });
+  if (!planet) res.send({ err: "no planet found" });
 
   planet.mines.push({
     clanId,
@@ -36,18 +36,18 @@ app.get('/addMiner', async (req, res) => {
 app.get('/mintPlanet', async (req, res) => {
   const account = req.query.to;
   const planetName = req.query.name;
-  
+
   const player = await playersSchema.findOne({ account });
 
-  if(!player) return res.send({ status: false });
-  if(player.data.minted.includes(planetName)) return res.send({ status: false });
-  
+  if (!player) return res.send({ status: false });
+  if (player.data.minted.includes(planetName)) return res.send({ status: false });
+
   const counter = await planetsSchema.findOne({ name: "counter" });
   try {
     await mintNft(planetName, counter.options.defaultOcupation + 1, account);
-  } catch(e) {
+  } catch (e) {
     console.log(e);
-    return res.send({ status: false });  
+    return res.send({ status: false });
   }
 
   player.data.minted.push(planetName);
@@ -68,7 +68,7 @@ app.get('/couldMint', async (req, res) => {
 
 app.get('/addIndex', async (req, res) => {
   const planet = await planetsSchema.findOne({ name: "counter" });
-  
+
   planet.options.defaultOcupation++;
   await planet.save();
 
@@ -87,15 +87,19 @@ app.get('/updatePlayer', async (req, res) => {
   const artifact2 = req.query.artifact2;
   const artifact3 = req.query.artifact3;
   const mint = req.query.mint;
-  
+
   const player = await playersSchema.findOne({ account });
-  if(!player) return res.send({ status: false });
-  
-  if(fuel) player.data.fuel = fuel;
-  if(artifact1) player.data.artifact1 = artifact1;
-  if(artifact2) player.data.artifact2 = artifact2;
-  if(artifact3) player.data.artifact3 = artifact3;
-  if(mint) player.data.minted.push(mint);
+  if (!player) return res.send({ status: false });
+
+  const newData = { ...player.data };
+
+  if (fuel) newData.fuel = Number(fuel);
+  if (artifact1) newData.artifact1 = Number(artifact1);
+  if (artifact2) newData.artifact2 = Number(artifact2);
+  if (artifact3) newData.artifact3 = Number(artifact3);
+  if (mint) newData.minted.push(mint);
+
+  player.data = newData;
   await player.save();
 
   res.send({});
@@ -104,8 +108,8 @@ app.get('/updatePlayer', async (req, res) => {
 app.get('/getPlayer', async (req, res) => {
   const account = req.query.account;
   const player = await playersSchema.findOne({ account });
-  if(!player) {
-    const newPlayer = new playersSchema({account});
+  if (!player) {
+    const newPlayer = new playersSchema({ account });
     await newPlayer.save();
     return res.send(newPlayer);
   }
